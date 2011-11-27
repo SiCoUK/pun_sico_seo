fb72455fa467391be27475fb2d85d03d7688a2b9
Simon Corless <simon@sico.co.uk>
2011-11-27 23:37:52 +0000

    Added language files.

---
+++ lang/English/sico_seo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * English Language file for sico_seo
+ *
+ * @copyright Copyright (C) 2011 Simon Corless
+ * @package sico_sea
+ */
+
+if (!defined('FORUM')) die();
+
+$lang_latest_posts = array(
+    
+);
